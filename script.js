63aeb433bfc71b68320d823f612927cff8b58359
CarlosRosas25 <cjrosasbrito@gmail.com>
2022-12-14 22:17:24 -0500

    Improve functionalities and styles

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-import products from "./sources/data.js";
+const requestData = async() => {
+  const resp = await fetch("./sources/data.json")
+  const data = await resp.json()
+  startProgram(data)
+}
+
+requestData()
+
 
 let cartList = []
-
 let categories = ["bollería", "repostería", "pastelería", "sánguches", "cafés", "repostería salada"]
 let categoryHTML = ""
 
-categories.forEach (category => {
+function startProgram (products) {
+  categories.forEach (category => {
   let productsPerCategory = products.filter ((product) => product.category === category)
 
   categoryHTML += `
   <div class="categoryCard">
-    <button class="categoryButton">
-      <h2>${category.toUpperCase()}</h2>
-    </button>
+    <div class="categoryTitle">
+      ${category}
+    </div>
     <ul>
       ${createProductsList()}
     </ul>
@@ -20,26 +27,49 @@ categories.forEach (category => {
   `
 
   function createProductsList () {
-    let productsList = ""
-    productsPerCategory.forEach ((el) => {
-      productsList += `
-      <li>
-        <span>${el.productName}</span>
-        <button id=${el.id} class="buttonAddToCart">Add to cart</button>
-      </li>
-      `
-    })
-    return productsList
-  }
-});
+      let productsList = ""
+      productsPerCategory.forEach ((el) => {
+        productsList += `
+        <li>
+          <span>${el.productName}</span>
+          <button id=${el.id} class="buttonAddToCart">Add to cart</button>
+        </li>
+        `
+      })
+      return productsList
+    }
+  });
 
-document.getElementById("container").innerHTML = categoryHTML
+  document.getElementById("container").innerHTML = categoryHTML
 
-document.addEventListener("click", (event) => {
-  if (event.target && event.target.className === "buttonAddToCart") {
-    cartList.push(products.find((product) => product.id == event.target.id))
-    
+  document.addEventListener("click", (event) => {
+    if (event.target && event.target.className === "buttonAddToCart") {
+      addingProduct(event)
+
+      Toastify({
+        text: "Producto agregado",
+        duration: 1000,
+        gravity: "bottom",
+        position: "right",
+        className: "info",
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+      }).showToast();
+    }
+  });
+
+  function addingProduct(event) {
+    let addedProduct = products.find(product => product.id == event.target.id)
+    let findingProduct = cartList.some(product => product.id == addedProduct.id)
+
+    if (findingProduct) {
+      addedProduct.quantity += 1
+    } else {
+      cartList.push(addedProduct)
+    }
+      
     let cartJSON = JSON.stringify(cartList)
     localStorage.setItem("cartItems", cartJSON)
   }
-})
+}
--- scriptCart.js
+++ scriptCart.js
@@ -2,63 +2,85 @@ let cartContainer = document.getElementById("productsList")
 let totalContainer = document.getElementById("totalPrice")
 
 let cartHTML = ""
+let parsedCartItems = JSON.parse(localStorage?.getItem("cartItems"))
+let cartArray = parsedCartItems ? parsedCartItems : []
 
-let cartArray = JSON.parse(localStorage?.getItem("cartItems"))
-
-if (cartArray){
-  cartArray.forEach(product => {
+function displayCart (cartProducts) {
+  if (cartProducts.length >= 1){
+    cartProducts.forEach(product => {
+      cartHTML += `
+      <div class="productName">${product.productName}</div>
+      <div>${product.cost}</div>
+      <div>${product.quantity}</div>
+      <div class="subtotal">${product.cost*product.quantity}</div>
+      <div><img id=${product.id} class="binIcon" src="./sources/images/bin.png" alt="bin"/></div>
+      `
+    }); 
+  } else {
     cartHTML += `
-    <div>${product.productName}</div>
-    <div>${product.cost}</div>
-    <div>1</div>
-    <div class="subtotal">${product.cost*1}</div>
-    <div><img id=${product.id} class="binIcon" src="./sources/images/bin.png" alt="bin"/></div>
+    <div class="emptyCart">Carrito vacío</div>
     `
-  }); 
-} else {
-  cartHTML += `
-  <div class="emptyCart">Carrito vacío</div>
+  }
+  cartContainer.innerHTML = cartHTML
+}
+
+displayCart(cartArray)
+
+function displayTotals () {
+
+  let subtotals = document.getElementsByClassName("subtotal")
+  let subTotal = 0
+  for (let i = 0; i < subtotals.length; i++) {
+    subTotal += Number(subtotals[i].innerText)
+  }
+
+  let igv = (18*subTotal)/100
+  let total = (subTotal+igv).toFixed(2)
+
+  totalContainer.innerHTML = `
+  <div class="one">Total de la compra</div>
+  <div class="two">Subtotal</div>
+  <div class="three">${subTotal}</div>
+  <div class="four">IGV</div>
+  <div class="five">${igv}</div>
+  <div class="six">Total</div>
+  <div class="seven">${total}</div>
+  <div class="totalBottons">
+    <button class="buttonCancel">Cancelar compra</button>
+    <button class="buttonPrintTicket">Emitir boleta</button>
+  </div>
   `
 }
 
-cartContainer.innerHTML = cartHTML
-
-let subtotals = document.getElementsByClassName("subtotal")
-let subTotal = 0
-for (let i = 0; i < subtotals.length; i++) {
-  subTotal += Number(subtotals[i].innerText)
-}
-
-let igv = (18*subTotal)/100
-let total = subTotal+igv
-
-totalContainer.innerHTML = `
-<div class="one">Total de la compra</div>
-<div class="two">Subtotal</div>
-<div class="three">${subTotal}</div>
-<div class="four">IGV</div>
-<div class="five">${igv}</div>
-<div class="six">Total</div>
-<div class="seven">${total}</div>
-<div class="totalBottons">
-  <button class="buttonCancel">Cancelar compra</button>
-  <button class="buttonPrintTicket">Emitir boleta</button>
-</div>
-`
-
+displayTotals()
 
 document.addEventListener("click", (event) => {
   if (event.target && event.target.className === "binIcon") {
     cartArray = cartArray.filter(product => product.id != event.target.id)
     let cartJSON = JSON.stringify(cartArray)
     localStorage.setItem("cartItems", cartJSON)
-    alert("Debe refrescar la página para ver los resultados.")
+    cartHTML = ""
+    displayCart(cartArray)
+    displayTotals()
   }
   if (event.target && event.target.className === "buttonPrintTicket") {
-    alert("Imprimiendo boleta")
+    Swal.fire({
+      title: 'Imprimiendo boleta',
+      icon: 'success',
+      timer: 2000,
+      showConfirmButton: false,
+    })
   }
   if (event.target && event.target.className === "buttonCancel") {
     localStorage.clear()
-    alert("Debe refrescar la página para ver los resultados.")
+    cartHTML = ""
+    displayCart([])
+    displayTotals()
+    Swal.fire({
+      title: 'Compra cancelada',
+      icon: 'error',
+      timer: 2000,
+      showConfirmButton: false,
+    })
   }
 })
